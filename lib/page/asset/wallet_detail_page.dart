a34eb314460c9a914bf8cfe63d5e54ded2f9d681
vincecfl <vincecfl@gmail.com>
2020-11-15 21:12:11 +0800

    update wallet detail

--- lib/page/asset/wallet_detail_page.dart
+++ lib/page/asset/wallet_detail_page.dart
@@ -1,8 +1,11 @@
 import 'package:flash_tron_wallet/common/color.dart';
 import 'package:flash_tron_wallet/provider/home_provider.dart';
 import 'package:flash_tron_wallet/router/application.dart';
+import 'package:flash_tron_wallet/util/common_util.dart';
 import 'package:fluro/fluro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -15,8 +18,17 @@ class WalletDetailPage extends StatefulWidget {
 }
 
 class _WalletDetailPageState extends State<WalletDetailPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name;
+  String _mnemonic;
+  String _pwd;
+
   @override
   Widget build(BuildContext context) {
+    HomeProvider homeProvider = Provider.of<HomeProvider>(context, listen: true);
+    _name = homeProvider.name;
+    _mnemonic = homeProvider.mnemonic;
+    _pwd = homeProvider.pwd;
     return Scaffold(
       backgroundColor: MyColors.lightBg,
       appBar: AppBar(
@@ -47,7 +59,7 @@ class _WalletDetailPageState extends State<WalletDetailPage> {
       body: Container(
         child: ListView(
           children: <Widget>[
-            IntervalPage(ScreenUtil().setHeight(25)),
+           //IntervalPage(ScreenUtil().setHeight(25)),
             _topWidget(context),
             IntervalPage(ScreenUtil().setHeight(25)),
             _backupMnemonicWidget(context),
@@ -150,7 +162,7 @@ class _WalletDetailPageState extends State<WalletDetailPage> {
   Widget _backupKeyWidget(BuildContext context) {
     return InkWell(
       onTap: () {
-        Application.router.navigateTo(context, 'asset/backupKey', transition: TransitionType.cupertino);
+        return _showInputPwdDialLog(context, 2);
       },
       child: Container(
         margin: EdgeInsets.only(left: ScreenUtil().setWidth(40), top: ScreenUtil().setHeight(30), right: ScreenUtil().setWidth(40)),
@@ -187,7 +199,7 @@ class _WalletDetailPageState extends State<WalletDetailPage> {
   Widget _backupMnemonicWidget(BuildContext context) {
     return InkWell(
       onTap: () {
-        Application.router.navigateTo(context, 'asset/backupMnemonic', transition: TransitionType.cupertino);
+        return _showInputPwdDialLog(context, 1);
       },
       child: Container(
         margin: EdgeInsets.only(left: ScreenUtil().setWidth(40), top: ScreenUtil().setHeight(30), right: ScreenUtil().setWidth(40)),
@@ -260,7 +272,9 @@ class _WalletDetailPageState extends State<WalletDetailPage> {
 
   Widget _delWalletWidget(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        return _showDelWalletDialLog(context);
+      },
       child: Container(
         margin: EdgeInsets.only(left: ScreenUtil().setWidth(40), top: ScreenUtil().setHeight(30), right: ScreenUtil().setWidth(40)),
         padding: EdgeInsets.only(bottom: ScreenUtil().setHeight(30)),
@@ -289,4 +303,154 @@ class _WalletDetailPageState extends State<WalletDetailPage> {
       ),
     );
   }
+
+  _showInputPwdDialLog(BuildContext context, int type) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+          title: Text(
+            '请输入密码',
+            style: TextStyle(
+                fontSize: ScreenUtil().setSp(30),
+                letterSpacing: 0.5,
+                color: Colors.grey[800],
+            ),
+          ),
+          content: Card(
+            elevation: 0.0,
+            child: Column(
+              children: <Widget>[
+                Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    onSaved: (String value) {
+                      if (value.length > 6) {
+                        value = value.substring(0, 6);
+                      }
+                      _pwd = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: '',
+                      filled: true,
+                      fillColor: Colors.white,
+                      //border:InputBorder.none,
+                    ),
+                    obscureText: true,
+                    maxLength: 6,
+                    maxLengthEnforced: true,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                    validator: (String value) {
+                      if (value.length < 6) {
+                        return '密码为6位';
+                      } else if (value.substring(0, 6) != _pwd) {
+                        return '密码不正确';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                '取消',
+                style: TextStyle(
+                    fontSize: ScreenUtil().setSp(30),
+                    letterSpacing: 0.5,
+                    color: Colors.blue[800],
+                ),
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+            FlatButton(
+                child: Text(
+                  '确定',
+                  style: TextStyle(
+                      fontSize: ScreenUtil().setSp(30),
+                    letterSpacing: 0.5,
+                    color: Colors.red[500],
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    _backupKey().then((val) {
+                      if (val == true) {
+                        Navigator.pop(context);
+                        if (type == 1) {
+                          Application.router.navigateTo(context, 'asset/backupMnemonic', transition: TransitionType.cupertino);
+                        } else if (type == 2) {
+                          Application.router.navigateTo(context, 'asset/backupKey', transition: TransitionType.cupertino);
+                        }
+                      } else {
+                        Util.showToast('执行出错，请再尝试');
+                      }
+                    });
+                  }
+
+                }
+            ),
+          ],
+        ));
+  }
+
+  _showDelWalletDialLog(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+          title: Text(
+            '请确保私钥或助记词已备份，确定删除?',
+            style: TextStyle(
+                fontSize: ScreenUtil().setSp(28),
+                letterSpacing: 0.5,
+                color: Colors.grey[800],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                '取消',
+                style: TextStyle(
+                    fontSize: ScreenUtil().setSp(30),
+                    letterSpacing: 0.5,
+                    color: Colors.blue[800],
+                ),
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+            FlatButton(
+                child: Text(
+                  '确定',
+                  style: TextStyle(
+                      fontSize: ScreenUtil().setSp(30),
+                      letterSpacing: 0.5,
+                      color: Colors.red[500],
+                  ),
+                ),
+                onPressed: () {
+                  _delWallet().then((val) {
+                    if (val == true) {
+                      Navigator.pop(context);
+                    } else {
+                      Util.showToast('执行出错，请再尝试');
+                    }
+                  });
+                }
+            ),
+          ],
+        ));
+  }
+
+  Future<bool> _backupKey() async {
+    return true;
+  }
+
+  Future<bool> _delWallet() async {
+    return await Provider.of<HomeProvider>(context, listen: false).delWallet();
+  }
+
 }
